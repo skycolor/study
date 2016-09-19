fb19bc17dd1aab0d6241395d23de6b441251f2ce
skycolor <873276172@qq.com>
2016-09-19 14:39:09 +0800

    module初次提交，浏览器还不兼容

---
+++ frontWeb/es6/module/exportDemo1.js
@@ -0,0 +1,12 @@
+export function getName(){
+    return 'hf';
+}
+export function getAge(){
+    return 26;
+}
+export function getAddress(){
+    return 'china';
+}
+export {
+    getName , getAge , getAddress
+}
---
+++ frontWeb/es6/module/importDemo1.js
@@ -0,0 +1,8 @@
+/*
+ * PS: 模块化开发，可以引入外部JS文件，然后调用里面的方法
+*/
+import * as tool from "./exprotDemo1";
+console.log(tool.getName());
+console.log(tool.getAge());
+console.log(tool.getAddress());
+
