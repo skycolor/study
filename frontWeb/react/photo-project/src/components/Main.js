4dd5d4afad3b4e01e4902172d66d3a3bcb204cdb
skycolor <873276172@qq.com>
2016-10-08 15:06:46 +0800

    加入ready备注，以及照片墙项目准备工作，图片、数据源、配置项等等

--- frontWeb/react/photo-project/src/components/Main.js
+++ frontWeb/react/photo-project/src/components/Main.js
@@ -10,8 +10,7 @@ class AppComponent extends React.Component {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <div>Test</div>
+        <div className="notice"></div>
       </div>
     );
   }
