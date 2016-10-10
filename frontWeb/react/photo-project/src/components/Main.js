8d473e097ba172b760d348a459c3f062198fe14f
skycolor <873276172@qq.com>
2016-10-10 17:57:38 +0800

    照片墙基本架子搭建，包括静态资源

--- frontWeb/react/photo-project/src/components/Main.js
+++ frontWeb/react/photo-project/src/components/Main.js
@@ -1,22 +1,81 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+/*
+* 获取图片的相关数据，并且初始化
+*/
+const imageDatas = ((arr) => {
+    for(let i = 0 , item ; item = arr[i++];)
+        item.imageURL = require('../images/' + item.fileName);
+    return arr;
+})(require('json!../data/imageDatas.json'))
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice"></div>
-      </div>
-    );
-  }
+/*
+* 单个图片组件
+*/
+class ImageFigure extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render(){
+        return (
+            <figure>
+                <img  src={this.props.data.imageURL}  alt={this.props.data.title} />
+                <figcaption>
+                    <h2>{this.props.data.title}</h2>
+                    <div>
+                        {this.props.data.desc}
+                    </div>
+                </figcaption>
+            </figure>
+        )
+    }
 }
 
-AppComponent.defaultProps = {
+/*
+* 底部导航条组件
+*/
+class ControllerUnit extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render(){
+        return (
+            <span class='unitClassName'></span>
+        )
+    }
+}
+
+/*
+* 页面主体
+*/
+class MainPhotoWall extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+        let imgFigures = [];
+        for(let i = 0 , figure; figure = imageDatas[i++];){
+            imgFigures.push(<ImageFigure data={figure}  />)
+        }
+
+        return (
+            <section>
+                <section className="index">
+                    {imgFigures}
+                </section>
+                <nav>
+
+                </nav>
+            </section>
+        );
+    }
+}
+
+
+MainPhotoWall.defaultProps = {
 };
 
-export default AppComponent;
+export default MainPhotoWall;
